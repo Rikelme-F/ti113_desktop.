4dcadec994c1c45a059ccc20069919744d639f39
Rikelme-F <Rikelme.fsantos@senacsp.edu.br>
2025-02-18 19:22:41 -0300

    Adicionar arquivos de projeto.

---
+++ EX2/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem = "";
+            double dia = 0;
+
+            Console.WriteLine("Informe um número de 1 a 7:");
+            dia = Convert.ToDouble(Console.ReadLine());
+
+            if (dia == 1)
+            {
+                mensagem = "Domingo";
+            }
+            else if (dia == 2)
+            {
+                mensagem = "Segunda-feira";
+            }
+            else if (dia == 3)
+            {
+                mensagem = "Terça-feira";
+            }
+            else if (dia == 4)
+            {
+                mensagem = "Quarta-feira";
+            }
+            else if (dia == 5)
+            {
+                mensagem = "Quinta-feira";
+            }
+            else if (dia == 6)
+            {
+                mensagem = "Sexta-feira";
+            }
+            else if (dia == 7)
+            {
+                mensagem = "Sábado";
+            }
+            else
+            {
+                mensagem = "Número inválido. Por favor, insira um número de 1 a 7.";
+            }
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
---
+++ StatusAluno/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace StatusAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1 = 0;
+            double nota2 = 0;
+            string mensagem = "";
+            double media = 0;
+
+            Console.Write("Informe a 1 nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            Console.Write("Infome a 2 nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                mensagem = "Aprovado";
+            }
+            else if (media >= 5 && media < 7)
+            {
+                mensagem = "Recuperação";
+            }
+            else
+            {
+                mensagem = "Reprovado";
+            }
+            Console.WriteLine($"O Status do Aluno de Média: {media} é {mensagem}");
+
+        }
+    }
+}
+
